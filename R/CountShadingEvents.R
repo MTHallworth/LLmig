573229d56ec2e68534dd2fdfd23f41ba8b3fcb5b
Mike Hallworth <mhallworth@gmail.com>
2017-02-13 20:43:28 -0500

    added CountShadingEvents function

--- R/CountShadingEvents.R
+++ R/CountShadingEvents.R
@@ -3,7 +3,6 @@
 #' @param lightdata an object with Date and Light.
 #' @param \code{threshold} threshold value
 #' @param \code{MCMCresult} either MCMC result from Estelle model or slices object
-#' @param \code{RiseToSet} logical if TRUE counts from sunrise to sunset
 #' @param \code{plot} logical plot results
 #'
 #' @export
@@ -11,35 +10,60 @@
 CountShadingEvents <- function(lightdata = lightdata,
                                threshold = 1,
                                MCMCresult = NA,
-                               RiseToSet = TRUE,
                                plot = TRUE){
-
+if(class(MCMCresult) == "slices"){
+Rises <- MCMCresult$mcmc[[1]]$time[MCMCresult$mcmc[[1]]$rise == TRUE]
+Sets <- MCMCresult$mcmc[[1]]$time[MCMCresult$mcmc[[1]]$rise == FALSE]
+}
+else{
 Rises <- MCMCresult$model$time[MCMCresult$model$rise == TRUE]
-
 Sets <- MCMCresult$model$time[MCMCresult$model$rise == FALSE]
+}
 
-if(min(Rises)<min(Sets)){
 nRise <- length(Rises)
 nSets <- length(Sets)
 
 shadesEvents <- array(NA,c(min(c(nRise,nSets)),2))
+colnames(shadesEvents)<-c("OrdinalDay","Shading_Events")
 
 shadesEvents[,1] <- format(Rises,"%j")[1:nrow(shadesEvents)]
-
+if(nRise > nSets){
 for(i in 1:nrow(shadesEvents)){
 shadesEvents[i,2] <- as.numeric(length(which(lightdata$Light[which(lightdata$Date > Rises[i] & lightdata$Date < Sets[i])] < threshold)))
 }
 }
-colnames(shadesEvents)<-c("OrdinalDay","Shading_Events")
-shadesEvents[,2]<-as.numeric(shadesEvents[,2])
+else{
+for(i in 1:nrow(shadesEvents)){
+shadesEvents[i,2] <- as.numeric(length(which(lightdata$Light[which(lightdata$Date > Rises[i+1] & lightdata$Date < Sets[i])] < threshold)))
+}
+}
 
-
-if(plot == TRUE){
-barplot(as.numeric(shadesEvents[,2]), yaxt = "n",ylab = "Number of shading events",width = 0.844)
+if(plot){
+barplot(as.numeric(shadesEvents[,2]),
+        yaxt = "n",
+        ylab = "Number of shading events",
+        width = 0.844)
 axis(2,las = 2)
 axis(1,label = as.Date(Rises[seq(1,nRise,10)]), at = seq(1,nRise,10),las = 2,cex.lab = 0.2)
 }
 
-return(shadesEvents)
+MoreRises <- data.frame(OrdinalDate = shadesEvents[,1],
+                        Rise = Rises[1:nrow(shadesEvents)],
+                        Set = Sets,
+                        ShadingEvents = shadesEvents[,2])
+if(nRise < nSets){
+MoreSets <- data.frame(OrdinalDate = shadesEvents[,1],
+                       Rise = Rises,
+                       Set = Sets[1:nrow(shadesEvents)],
+                       ShadingEvents = shadesEvents[,2])
 }
+
+if(nRise > nSets){
+return(MoreRises)
+} else {
+  return(MoreSets)
+}
+
+}
+
 
