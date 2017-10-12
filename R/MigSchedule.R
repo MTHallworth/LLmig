6003b82b7c80a3793a5fedd0fbff57c923506154
MTHallworth <mhallworth@gmail.com>
2017-10-11 21:32:46 -0400

    colors no legend

--- R/MigSchedule.R
+++ R/MigSchedule.R
@@ -424,9 +424,6 @@ plot(sp::SpatialPoints(cbind(lonlat$Median.lon,lonlat$Median.lat)),
      col = colors$color,
 	   add = TRUE)
 box()
-par(new = TRUE, fig = c(0,0.2,0.8,1))
-pie(as.numeric(month), labels = col.dat$month, col = col.dat$color)
-
 
 # Plot Stop-over locations #
 cols <- sp::bpy.colors(nrow(movementResult))
